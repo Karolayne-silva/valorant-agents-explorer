dfffbdc856060804df0d8471a6a18fd06b3d02a3
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2024-01-22 22:50:33 -0300

    ajuste para mostrar os dados da requisição

--- src/componentes/CardHabilidade.js
+++ src/componentes/CardHabilidade.js
@@ -1,15 +1,16 @@
 
-export default function CardHabilidade({img, nome, descricao}){
+export default function CardHabilidade({img, nome, descricao, letra}){
+    
     return(
         <div className="cardHabilidade">
             <div className="nome">
-                <img src={img} alt=""/>
+                <img src={img} alt={`icone do ${nome}`}/>
                 <h1>{nome}</h1>
             </div>
             <div className="descricao">
                 <p>{descricao}</p>
             </div>
-            <span className="letra">C</span>
+            <span className="letra">{letra}</span>
         </div>
     )
 }
--- src/pages/Agente/index.jsx
+++ src/pages/Agente/index.jsx
@@ -9,53 +9,67 @@ export default function Agente() {
   const { id } = useParams();
   const [dataAgente, setDataAgente] = useState([]);
 
+  console.log(id);
+  
   useEffect(() => {
     async function fetchApi() {
-      const response = await fetch(
-        `https://valorant-api.com/v1/agents/${id}?isPlayableCharacter=truelanguage='pt-BR'`
-      );
-      const json = await response.json();
-      setDataAgente(json.data);
+      try {
+        const response = await fetch(
+          `https://valorant-api.com/v1/agents/${id}?isPlayableCharacter=true&language=pt-BR`
+        );
+        const json = await response.json();
+        setDataAgente(json.data);
+      } catch (error) {
+        console.log(error);
+      }
     }
     fetchApi();
   }, [id]);
 
+  const letras = ['C', 'Q', 'E', 'X'];
   return (
     <>
       <Header />
       <main className="main">
         <section className="fundo-titulo">
-          <h1>{dataAgente.displayName}</h1>
+          <h1>{dataAgente?.displayName}</h1>
         </section>
 
         <section className="info-agente">
-          <p>{dataAgente.description}</p>
+          <p>{dataAgente?.description}</p>
           <div className="role">
-            {/* <img src={dataAgente.role.displayIcon} alt="" />
-            <span>{dataAgente.role.displayName}</span> */} 
+            <img src={dataAgente?.role?.displayIcon} alt="" />
+            <span>{dataAgente?.role?.displayName}</span>
           </div>
         </section>
 
         <section className="imgs">
           <div className="img-agente">
-            <img src={dataAgente.fullPortrait} alt="" />
+            <img src={dataAgente?.fullPortrait} alt="" />
             <div className="paleta">
-                 {dataAgente.backgroundGradientColors.map((cor, index) => (
+              {dataAgente?.backgroundGradientColors?.map((cor, index) => (
                 <div key={index} style={{ backgroundColor: `#${cor}` }}></div>
-              ))}    
+              ))}
             </div>
           </div>
         </section>
 
         <section className="habilidades">
-             {dataAgente.abilities.map((habilidade, index) => (
-            <CardHabilidade
-              key={index}
-              img={habilidade.displayIcon}
-              nome={habilidade.displayName}
-              descricao={habilidade.description}
-            />
-          ))}   
+
+          {
+          dataAgente?.abilities
+            ?.filter((habilidade) => habilidade.slot !== "Passive")
+            .map((habilidade, index) => (
+              
+              <CardHabilidade
+                key={index}
+                img={habilidade.displayIcon}
+                nome={habilidade.displayName}
+                descricao={habilidade.description}
+                letra={letras[index]}
+               
+              />
+            ))}
         </section>
       </main>
 
